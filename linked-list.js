092ff6b10306e179dc56442cafb288b336f58397
Benjamin Renker <brenker@theworkshop.com>
2024-10-23 17:10:53 +0200

    add contains find toString insertAt removeAt and fix prepend to account for empty list also use tests from odin project in main

--- linked-list.js
+++ linked-list.js
@@ -20,7 +20,10 @@ const linkedList = () => {
 
     const prepend = (value) => {
         let newNode = node(value, head.next, head);
-        head.next.prev = newNode;
+        if(head.next !== null) {
+            // When the list is not empty
+            head.next.prev = newNode;
+        }
         head.next = newNode;
     }
 
@@ -77,11 +80,85 @@ const linkedList = () => {
     }
 
     const pop = () => {
-        let temp = head
+        let temp = head;
         while(temp.next !== null) {
             temp = temp.next;
         }
         temp.prev.next = null;
+    }
+
+    const contains = (value) => {
+        let isContained = false;
+        let temp = head;
+        while(temp.next !== null && !isContained) {
+            temp = temp.next;
+            if(temp.value === value) {
+                isContained = true;
+            }
+        }
+        return isContained;
+    }
+
+    const find = (value) => {
+        let index = -1;
+        let temp = head;
+        let isContained = false;
+        while(temp.next !== null && !isContained) {
+            temp = temp.next;
+            if(temp.value === value) {
+                isContained = true;
+            }
+            index++;
+        }
+        if(isContained) {
+            return index;
+        } else {
+            return -1;
+        }
+    }
+
+    const toString = () => {
+        let outStr = "";
+        let temp = head;
+        while(temp.next !== null) {
+            temp = temp.next;
+            outStr += `( ${temp.value} ) -> `;
+        }
+        console.log(`${outStr} null`)
+    }
+
+    const insertAt = (value, i) => {
+        /*head -> prev|ONE|next -> prev|TWO|next -> prev|THREE|next -> null */
+        //                                              t, i = 0
+        let temp = head;
+        while(temp.next !== null && i > 0) {
+            temp = temp.next;
+            i--;
+        }
+        let newNode = node(value, temp.next, temp);
+        if(temp.next !== null) {
+            // Only if not inserting at the end
+            temp.next.prev = newNode;
+        }
+        temp.next = newNode;
+    }
+
+    const removeAt = (i) => {
+    /*head -> prev|ONE|next -> prev|TWO|next -> prev|THREE|next -> null */
+    // t, i = 0
+        let temp = head;
+        if(temp.next === null) {
+            throw new Error("Can't delet form empty list");
+        }
+        while(temp.next !== null && i > 0) {
+            temp = temp.next;
+            i--;
+        }
+        temp.next = temp.next.next;
+        if(temp.next !== null) {
+            // only if elem is not at the end
+            temp.next.prev = temp;
+        }
     }
 
     return {
@@ -94,6 +171,11 @@ const linkedList = () => {
         tail: tail,
         at: at,
         pop: pop,
+        contains: contains,
+        find: find,
+        toString: toString,
+        insertAt: insertAt,
+        removeAt: removeAt, 
     }
 
 }
--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 import linkedList from './linked-list.js'
 
-const myList = linkedList();
-myList.append("two");
-myList.prepend("one");
-myList.append("three");
-myList.printValues();
-// console.log(myList.size());
-// console.log(myList.first());
-// console.log(myList.tail());
-// console.log(myList.at(3));
-myList.pop();
-myList.printValues();
+const list = linkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+list.toString();
